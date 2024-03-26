7ce620e9e65b746dcfcd8dedec1cfe677be4cadc
Aditya Kumar <135034451+Adimgr1@users.noreply.github.com>
2024-03-26 07:43:53 +0000

    datatypes

--- basics.js
+++ basics.js
@@ -1,1 +1,7 @@
 console.log("Aditya");
+let a;
+const b =20;
+c=20
+let d;
+let e;
+console.table([a,b,c,d,e ])
---
+++ datatypes/data.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) // we are using nodejs, not browser
+
+var a= "Hello"
+var b = 22.45;
+var c = 2.0000017637831;
+var d = true
+var e;
+console.log(typeof a + " " + typeof b + " " +typeof c + " " + typeof e);
+null // not a type but a value or object;
+undefined // a type. variables whose value is not initialized  is of type undefined.
+Symbol // used in react to give uniqueness to the componenets;
+
+// primitive data types -
+//  numbers - integers and decimals both are of number types
+// number data type have some capacity if it crosses that then we have bigint.
+//  String
+//  Boolean
+//  Symbol
+//  undefined
+
+//then we have object data type
