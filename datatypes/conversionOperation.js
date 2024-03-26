4b3ebb00b9ca8efd4ae20f77e9a3332a5b5f39ed
Aditya Kumar <135034451+Adimgr1@users.noreply.github.com>
2024-03-26 09:58:01 +0000

    operation

--- datatypes/conversionOperation.js
+++ datatypes/conversionOperation.js
@@ -1,7 +1,86 @@
-let a = 33;
+// let a = 33;
+// let b = 0.1;
+// let c = true;
+// let d = false;
+// let e = undefined
+// let f = null
 // whenever we want to convert any primitive data type to another data type we write the target data type starting 
 //with capital letter;
 //like here a is number and we converting it to string;
 
-let toString = String(a)
-console.log(toString + " " + typeof toString)
+// let atoString = String(a)
+// console.log(atoString + " " + typeof atoString)
+// let btoString = String(b)
+// console.log(btoString + " " + typeof btoString)
+// let ctoString= String(c)
+// console.log(ctoString + typeof ctoString)
+// let etoString=String(e)
+// console.log(etoString + " " + typeof etoString)
+// let ftoString=String(f)
+// console.log(ftoString + " "+ typeof ftoString)
+
+// we have seen above that any data type can easily be converted into string.
+// note- undefined and null is also cconverted to string.
+// toNumber conversion-
+// let a = "";
+// let b = "33 abc";
+// let c = "abc";
+// let d = true;
+// let e = false
+// let f = null
+// let g ;
+
+// let atonumber = Number(a)
+// console.log(atonumber + " " + typeof atonumber)
+
+// let btonumber = Number(b)
+// console.log(btonumber + " " + typeof btonumber)
+
+// let ctonumber= Number(c)
+// console.log(ctonumber + " " + typeof ctonumber)
+
+// let dtoNumber = Number(d)
+// console.log(dtoNumber + " " + typeof dtoNumber);
+
+// let etonumber=Number(e)
+// console.log(etonumber + " " + typeof etonumber)
+
+// let ftonumber=Number(f)
+// console.log(ftonumber + " "+ typeof ftonumber)
+
+// let gtonumber = Number(g)
+// console.log(gtonumber = " " + typeof gtonumber)
+
+// any string which contains numeric value can easily converted to number
+// but if any string  is empty then its number conversion is 0;
+// if any string contains alphabets then its converted value is NaN(Not a Number)
+// undefined can be converted to number but value is nothing.
+// null can be converted to number value is 0;
+//true is one and false is 0 is numbers.
+
+// ***********OPETRATIONS **********
+
+// addition 
+// console.log(3+3);
+// //sub
+// console.log(3-3);
+// //mul
+// console.log(3*3);
+// // power
+// console.log(3**3);
+// // division 
+// console.log(3/3);
+// // remainder 
+// console.log(3%3);
+
+//console.log("3" +3); // string concatenation
+//console.log("3" + 2+2); // string concatenation
+ var a= 1;
+ console.log(++a); //prefix operator- here first the value of a is increased then print happens
+ console.log(a++); // postfix operator - here first the value of b is printed then increased,
+ console.log(a);
+
+
+
+
+
