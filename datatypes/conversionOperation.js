10fcf4fb72b63ba129cab908e0ad4278b1e72fc1
Aditya Kumar <135034451+Adimgr1@users.noreply.github.com>
2024-03-26 08:13:21 +0000

    operation

---
+++ datatypes/conversionOperation.js
@@ -0,0 +1,7 @@
+let a = 33;
+// whenever we want to convert any primitive data type to another data type we write the target data type starting 
+//with capital letter;
+//like here a is number and we converting it to string;
+
+let toString = String(a)
+console.log(toString + " " + typeof toString)
